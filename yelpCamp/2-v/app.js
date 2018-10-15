65910d2f8b69c0da6c9b59d43815f1cf9be265ac
seen2 <tiwarietc@gmail.com>
2018-10-15 13:18:09 +0530

    added mongo db in yelp camp

--- yelpCamp/2-v/app.js
+++ yelpCamp/2-v/app.js
@@ -1,11 +1,24 @@
-const express=require('express');
-const bodyParser=require('body-parser');
+const express=require('express'),
+      bodyParser=require('body-parser'),
+      mongoose=require('mongoose');
 
-
+mongoose.connect("mongodb://localhost:27017/yelpCamp", { useNewUrlParser: true })
+				/*
 				let data=[
 								{name:'alpha-0',img:'https://www.mercurynews.com/wp-content/uploads/2018/07/SJM-RESTOCAMP-0712-1.jpg?w=620',},
 								{name:'alpha-1',img:'https://www.mercurynews.com/wp-content/uploads/2018/07/SJM-RESTOCAMP-0712-3.jpg?w=620'}
 				]
+				
+				*/
+
+var YelpCampSchema=new mongoose.Schema(
+				{
+								name:String,
+								img:String
+				}
+);
+
+var camp=mongoose.model("camp",YelpCampSchema);
 
 const app=express();
 app.use(bodyParser.urlencoded({extended:true})); //used for post request to get body in request
@@ -18,14 +31,41 @@ app.get("/",(req,res)=>{
 });
 
 app.get("/camp",(req,res)=>{
-				res.render('campground',{data});
+				
+				camp.find({},(err,data)=>{
+								if(err){
+												console.log("something went wrong");
+								}else{
+												res.render('campground',{data});
+								}
+				});
+								
 });
 
 app.post("/camp",(req,res)=>{
 
 				let newCamp=req.body;
-				data.push(newCamp);
-				res.render('campground',{data});
+				//data.push(newCamp);
+				//saving data to data base
+
+				camp.create(newCamp,(err,data)=>{
+								if(err){
+												console.log(err);
+								}
+								else{
+												console.log("data saved!!");
+								}
+				});
+				//retrieving data from data base
+				camp.find({},(err,data)=>{
+								if(err){
+												console.log(err);
+								}
+								else{
+												res.render('campground',{data});
+								}
+				});
+				
 });
 
 app.get("/camp/new",(req,res)=>{
