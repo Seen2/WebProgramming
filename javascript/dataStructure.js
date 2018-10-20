e1636d0b39e48452eeca6d78219d944f1a4d30c1
seen2 <tiwarietc@gmail.com>
2018-10-21 03:14:47 +0530

    worked with list

---
+++ javascript/dataStructure.js
@@ -0,0 +1,18 @@
+arr=[()=>console.log("hello!"),"shintu",9,"S"];
+arr[0]();
+
+var arrMethods=()=>{
+				console.log("array length:"+arr.length);
+				console.log(arr);
+				console.log(arr.shift())
+				console.log("after shift:"+arr);
+				arr.pop()
+				console.log("after pop:"+arr);
+				arr.push(()=>console.log(9))
+				console.log("after push:"+arr);
+				arr.unshift("s");
+				console.log("after unshift:"+arr);
+
+}
+
+arrMethods();
