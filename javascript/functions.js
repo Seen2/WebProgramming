a4a3a4875ea03bcce1995f36b0a9dd4c05b02f53
seen2 <tiwarietc@gmail.com>
2018-10-21 02:58:01 +0530

    type of functions

---
+++ javascript/functions.js
@@ -0,0 +1,25 @@
+function sayHello(s="Dear"){
+				console.log("hello "+s);
+}
+//simple function calls
+sayHello();
+sayHello("shintu");
+
+             
+const greet=function(s="Dear"){ //anonomys function.
+				console.log("hello "+s);
+}
+
+//anonomys function assigned to greet get called
+greet();
+greet("shintu");
+
+//anonomys function
+
+greetHim=(s="Dear")=>{
+				console.log("hello "+s);
+}
+
+greetHim();
+greetHim("Shintu")
+
