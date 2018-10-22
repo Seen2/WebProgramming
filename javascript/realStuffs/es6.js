ddc57f3639f7fb06208ff335bc17c60ac463d4ad
seen2 <tiwarietc@gmail.com>
2018-10-22 22:46:16 +0530

    answered code

--- javascript/realStuffs/es6.js
+++ javascript/realStuffs/es6.js
@@ -1,6 +1,8 @@
-var a = 'test';
-var b = true;
-var c = 789;
+// change everything below to the newer Javascript!
+//
+// // let + const
+//
+let a = 'test',b = true,c = 789;
 a = 'test2';
 
 // Destructuring
@@ -11,39 +13,30 @@ var person = {
 				eyeColor  : "blue"
 };
 
-var firstName = person.firstName;
-var lastName = person.lastName;
-var age = person.age;
-var eyeColor = person.eyeColor;
+let {firstName , lastName , age , eyeColor } =person;
 
 
 // Object properties
-var a = 'test';
-var b = true;
-var c = 789;
+a = 'test';
+b = true;
+c = 789;
 
-var okObj = {
-				a: a,
-				b: b,
-				c: c
-};
+var okObj = {a,b,c,};
 
 
 // Template strings
-var message = "Hello " + firstName + " have I met you before? I think we met in " + city + " last summer no???";
-
-
+var message = `Hello ${ firstName} have I met you before? I think we met in ${city} last summer no???`;
 // default arguments
 // default age to 10;
-function isValidAge(age) {
+function isValidAge(age=10) {
 				return age
 }
 
 // Symbol
 // Create a symbol: "This is my first Symbol"
-
+const symbol=Symbol("This is my first Symbol");
 // Arrow functions
-function whereAmI(username, location) {
+whereAmI=(username, location)=> {
 				if (username && location) {
 								return "I am not lost";
 				} else {
