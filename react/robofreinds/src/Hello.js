14af786c17702589ab9e1f3400cb82d336932743
seen2 <tiwarietc@gmail.com>
2018-10-23 23:59:55 +0530

    hello world in react js

---
+++ react/robofreinds/src/Hello.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import 'tachyons';
+
+export default class App extends Component {
+  render() {
+    return (
+			<div className="f3 tr">
+				<h1> Hello! World..</h1>
+			</div>
+		);
+  }
+}
+
+
+/*
+ 
+import React, { Component } from 'react';
+import 'tachyons';
+
+const Hello= ()=>{
+    return (
+			<div className="f3 tr">
+				<h1> Hello! World..</h1>
+			</div>
+		);
+}
+export default Hello
+*/
